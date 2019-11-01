26fc77012fa0ecdef2fe3d48d3ff13b648f3cdb2
Carlos Mendez <cmendezlopez25@hotmail.com>
2019-10-31 20:33:34 -0400

    Added input validation in front-end.

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -7,6 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginEmail = "";
+  loginPassword = "";
+  loginSuccess = true;
+
+  registerEmail = "";
+  registerPassword = "";
+  confirmPassword = "";
+  firstName = "";
+  lastName = "";
+  registerSuccess = true;
+
+  login(): boolean{
+    if (this.loginEmail.length === 0 || this.loginPassword.length === 0){
+      this.loginSuccess = false;
+      return this.loginSuccess;
+    }
+
+    this.loginSuccess = true;
+    return this.loginSuccess;
+  }
+
+  register(): boolean{
+    if (this.registerEmail.length === 0 || this.registerPassword.length === 0 || this.confirmPassword.length === 0 || this.firstName.length === 0 || this.lastName.length === 0){
+      this.registerSuccess = false;
+      return this.registerSuccess;
+    }
+
+    if (!this.registerEmail.includes('@')) {
+      this.registerSuccess = false;
+      return this.registerSuccess;
+    }
+
+    if(this.registerPassword !== this.confirmPassword){
+      this.registerSuccess = false;
+      return this.registerSuccess;
+    }
+
+    this.registerSuccess = true;
+    return this.registerSuccess;
+  }
+
   constructor() { }
 
   ngOnInit() {
