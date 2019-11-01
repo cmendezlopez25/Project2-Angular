585fdd95864f59c02dc1335602b4f228f886edeb
Jane <thejaneshin@gmail.com>
2019-10-31 21:18:40 -0400

    changed modal ui

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -8,13 +8,25 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ModalComponent implements OnInit {
   @Input() public modalTitle = "";
 
-  public memberNames = ["Ricky@email.com", "Jane@email.com", "Jacob@email.com"];
-
-
+  public members = [
+    {email: "Ricky@email.com", role: "Owner"}, 
+    {email: "Jane@email.com", role: "Admin"},
+    {email: "Jacob@email.com", role: "Moderator"},
+  ];
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
+  public deleteAccount() {
+    // We'll replace the inner logic with account deletion http request
+    if (confirm("Are you sure you want to delete " + this.modalTitle + "?")) {
+      console.log("yes");
+    }
+    else {
+      console.log("no");
+    }
+  }
 }
