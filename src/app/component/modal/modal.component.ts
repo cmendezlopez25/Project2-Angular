e1a34ead5dcc614a880844d389c4c8747d491d17
Carmichael <carmichael901230@gmail.com>
2019-11-01 13:05:16 -0400

    test modal

--- src/app/component/modal/modal.component.ts
+++ src/app/component/modal/modal.component.ts
@@ -1,32 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, Input} from '@angular/core';
+
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements OnInit {
-  @Input() public modalTitle = "";
+export class ModalComponent {
+  closeResult: string;
+  @Input() public modalTitle="";
 
-  public members = [
-    {email: "Ricky@email.com", role: "Owner"}, 
-    {email: "Jane@email.com", role: "Admin"},
-    {email: "Jacob@email.com", role: "Moderator"},
-  ];
+  constructor(private modalService: NgbModal) {}
 
-  constructor() { }
-
-  ngOnInit() {
-    
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
   }
 
-  public deleteAccount() {
-    // We'll replace the inner logic with account deletion http request
-    if (confirm("Are you sure you want to delete " + this.modalTitle + "?")) {
-      console.log("yes");
-    }
-    else {
-      console.log("no");
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
     }
   }
 }
