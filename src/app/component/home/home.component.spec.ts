c0784c6ad034aecc5e3bdf30ec92fe8f22d473e3
Carmichael <carmichael901230@gmail.com>
2019-11-04 20:31:58 -0500

    angular test

--- src/app/component/home/home.component.spec.ts
+++ src/app/component/home/home.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { AddModalComponent } from '../add-modal/add-modal.component';
+import { ModalComponent } from '../modal/modal.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +12,13 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ 
+        HomeComponent,
+        SidebarComponent,
+        NavbarComponent,
+        AddModalComponent,
+        ModalComponent
+      ]
     })
     .compileComponents();
   }));
--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
