cce6be828d0b02669012ea4aa7632e0199e8009e
Carmichael <carmichael901230@gmail.com>
2019-11-05 21:58:55 -0500

    validation of login and register

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -8,65 +8,71 @@ import { User } from 'src/app/user';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  loginSuccess = false;
+  loginErrorMsg = "";
+  registerErrorMsg = "";
 
   loginUser = new User();
   registerUser = new User();
 
-  registerSuccess = false;
-
-  login(user: User): boolean{
-    if (user.email.length === 0 || user.password.length === 0){
-      console.log('inside if statement with length === 0');
-      this.loginSuccess = false;
-      return this.loginSuccess;
+  login(user: User): void{
+    console.log("1");
+    if (!user.email || !user.password) {
+      this.loginErrorMsg = "Email or password can't be empty.";
+      return;
     }
 
-    if (!user.email.includes('@')) {
-      this.loginSuccess = false;
-      return this.loginSuccess;
+    else if (!user.email.includes('@')) {
+      this.loginErrorMsg = "Email must contain @ symbol.";
+      return;
     }
-
-
-    this.loginSuccess = true;
-    console.log(this.loginSuccess);
+  
     this.loginService.loginUser(user)
       .subscribe(
-        res => console.log(res),
-        err => console.log(err)
+        res => {
+          if (!res) {
+            console.log("res is null")
+            this.loginErrorMsg = "The email and password did not match records.";
+            console.log(this.loginErrorMsg);
+          }
+          else {
+            console.log(res);
+          }
+        },
+        err => {
+          this.loginErrorMsg = "Failed to contact server.";
+        }
       );
-    return this.loginSuccess;
   }
 
-  register(user: User): boolean{
-    if (this.registerUser.email.length === 0 || 
-        this.registerUser.password.length === 0 || 
-        this.registerUser.confirmPassword.length === 0 || 
-        this.registerUser.firstName.length === 0 || 
-        this.registerUser.lastName.length === 0){
-      this.registerSuccess = false;
-      return this.registerSuccess;
+  register(user: User): void{
+    if (!this.registerUser.email || 
+        !this.registerUser.password || 
+        !this.registerUser.confirmPassword || 
+        !this.registerUser.firstName || 
+        !this.registerUser.lastName){
+      this.registerErrorMsg = "None of the fields can be empty."
+      return;
     }
 
     if (!this.registerUser.email.includes('@')) {
-      this.registerSuccess = false;
-      return this.registerSuccess;
+      this.registerErrorMsg = "Email must contain @ symbol.";
+      return;
     }
 
     if(this.registerUser.password !== this.registerUser.confirmPassword){
-      this.registerSuccess = false;
-      return this.registerSuccess;
+      this.registerErrorMsg = "Passwords do not match.";
+      return;
     }
 
     this.loginService.registerUser(user)
       .subscribe(
-        res => console.log(res),
-        err => console.log(err)
+        res => {
+          console.log(res)
+        },
+        err => {
+          console.log(err)
+        }
       );
-
-    this.registerSuccess = true;
-    return this.registerSuccess;
   }
 
   constructor(private loginService: LoginService) { }
